9af632a04244df396fc4e141405f5e728dc409ba
Daniel Leite <daniel199257@gmail.com>
2021-10-26 19:37:24 -0300

    Add filter api data

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -1,22 +1,21 @@
 const { requestDataJson } = require("../services/Request")
 
 const coinsSupplySmall = async (request, response) => {
-    
+
+    console.log(request.query)
+
     const resultCoinsRequest = await requestDataJson(process.env.URL_REQUEST)
-    
-    const coinsDataAnalises = []
 
-    for(let valueResultCoin of resultCoinsRequest['data']){
-        if(valueResultCoin['quote']['BRL']['price'] <= process.env.PRICE 
-        && valueResultCoin['max_supply'] <= process.env.MAX_SUPPLY 
-        && valueResultCoin['max_supply'] !== null){
-            coinsDataAnalises.push(valueResultCoin)
-        }
-    }
+    const coinsDataAnalises = resultCoinsRequest['data']
+        .filter(filterPrice => filterPrice['quote']['BRL']['price'] <= request.query.price)
+        .filter(filterSupply => filterSupply['max_supply'] <= request.query.supply)
+        .filter(filterNoNull => filterNoNull['max_supply'] !== null)
+        .filter(filterNoNull => filterNoNull['max_supply'] !== 0)
+        .filter(filterNoNull => filterNoNull['max_supply'] !== "")
+        .filter(filterNoNull => filterNoNull['max_supply'] !== undefined)
+    // console.log(coinsDataAnalises)
 
-    console.log(coinsDataAnalises)
-
-    response.status(200).json({resultCoinsFilter: coinsDataAnalises})
+    response.status(200).json({ resultCoinsFilter: coinsDataAnalises })
 
 }
 
